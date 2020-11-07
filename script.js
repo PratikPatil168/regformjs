c3fde9b0319da6154ec2d9daf1df9ade19bf3a28
PratikPatil168 <63632241+PratikPatil168@users.noreply.github.com>
2020-11-07 21:37:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,100 @@
-console.log("hello")
-var x = document.getElementById("tab");
+const submitBtn = document.getElementById("submitBtn");
 
-x.style.display = "none";
+submitBtn.addEventListener("click", validateForm);
 
 
-function calljs()
-{
-    var name = document.getElementById("fname").value+document.getElementById("lname").value;   
-    var phone = document.getElementById("cno").value;
-    var dob = document.getElementById("dob").value;
-    var email = document.getElementById("email").value;
-    var addr = document.getElementById("addr").value;
-    console.log(name.toString());
-    console.log(phone.toString());
-    console.log(dob.toString());
-    console.log(email.toString());
-    console.log(addr.toString());
-    var table = document.getElementById("table");
-    var newRow = table.insertRow(table.length);
-    col1 = newRow.insertCell(0);
-    col2 = newRow.insertCell(1);
-    col3 = newRow.insertCell(2);
-    col4 = newRow.insertCell(3);
-    col5 = newRow.insertCell(4);
-    col6 = newRow.insertCell(5);
-    var gender = "";
-    if (document.getElementById('Male').checked) 
-    {
-      gender="Male";
-    }
-    else if(document.getElementById('Female').checked)
-    {
-      gender="Female";
-    }
-    else if(document.getElementById('Other').checked)
-    {
-      gender="Other";
-    }
-    col1.innerHTML = name.toString();
-    col2.innerHTML = phone.toString();
-    col3.innerHTML = dob.toString();
-    col4.innerHTML = gender.toString();
-    col5.innerHTML = email.toString();
-    col6.innerHTML = addr.toString();
-    document.getElementById("forms").reset();
-
-};
-function jspage() {
-
- var x = document.getElementById("tab");
- var f = document.getElementById("fm");
- var elem = document.getElementById("jsp");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    f.style.display = "none";
-    elem.innerHTML = "Register Now" 
-
-  } else {
-    x.style.display = "none";
-    f.style.display = "block";
-    elem.innerHTML = "Show Registeration"
-  }
+// Defining a function to display error message
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
 }
+
+// Defining a function to validate form 
+function validateForm() {
+    // Retrieving the values of form elements 
+    var name = document.contactForm.name.value;
+    var email = document.contactForm.email.value;
+    var mobile = document.contactForm.mobile.value;
+    var gender = document.contactForm.gender.value;
+    var pass = document.contactForm.password.value;
+
+   
+	// Defining error variables with a default value
+    var nameErr = emailErr = mobileErr = passErr =  genderErr = true;
+    
+    // Validate name
+    if(name == "") {
+        printError("nameErr", "Please enter your name");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;                
+        if(regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid name");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+    }
+    
+    // Validate email address
+    if(email == "") {
+        printError("emailErr", "Please enter your email address");
+    } else {
+        // Regular expression for basic email validation
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+        } else{
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+    
+    // Validate mobile number
+    if(mobile == "") {
+        printError("mobileErr", "Please enter your mobile number");
+    } else {
+        var regex = /^[7-9]\d{9}$/;
+        if(regex.test(mobile) === false) {
+            printError("mobileErr", "Please enter a valid 10 digit mobile number");
+        } else{
+            printError("mobileErr", "");
+            mobileErr = false;
+        }
+    }
+    
+   
+    // Validate gender
+    if(gender == "") {
+        printError("genderErr", "Please select your gender");
+    } else {
+        printError("genderErr", "");
+        genderErr = false;
+    }
+
+
+    if(pass == "") {
+        printError("passErr", "Please enter strong password");
+    } else {
+        // Regular expression for basic Password validation
+        var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+        if(regex.test(pass) === false) {
+            printError("passErr", "Please enter a valid password ");
+        } else{
+            printError("passErr", "");
+            passErr = false;
+        }
+    }
+    // Prevent the form from being submitted if there are any errors
+    if((nameErr || emailErr || mobileErr || genderErr) == true) {
+       return false;
+    } else {
+        // Creating a string from input data for preview
+        var dataPreview = "You've entered the following details: \n" +
+                          "Full Name: " + name + "\n" +
+                          "Email Address: " + email + "\n" +
+                          "Mobile Number: " + mobile + "\n" +
+                          "Gender: " + gender + "\n";
+
+        // Display input data in a dialog box before submitting the form
+        alert(dataPreview);
+    }
+};
